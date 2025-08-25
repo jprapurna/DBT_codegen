7792c509d1b7e0896b4e7274b6912e1e12b95a50
jprapurna <prapurnajagarlamudi@gmail.com>
2025-08-25 18:01:45 +0530

    Added DBT Project

--- F92157/models/marts/int_tfarmers_state_lookup.sql
+++ F92157/models/marts/int_tfarmers_state_lookup.sql
@@ -2,10 +2,10 @@ WITH farmers_state AS (
     SELECT
         FARMERS_STATE_CD,
         STATE_CODE
-    FROM {{ ref('ref_tfarmers_state') }}
+    FROM {{ source('FDR', 'REF_TFARMERS_STATE') }}
 )
 SELECT
     FARMERS_STATE_CD,
     CAST(FARMERS_STATE_CD AS INTEGER) AS IFARMERS_STATE_CD,
     STATE_CODE
-FROM farmers_state;
+FROM farmers_state

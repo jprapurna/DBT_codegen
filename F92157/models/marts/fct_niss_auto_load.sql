5e5fe8375ebae5f0daa14906be1b3604c5317d98
jprapurna <prapurnajagarlamudi@gmail.com>
2025-08-25 17:50:30 +0530

    Added DBT project

--- F92157/models/marts/fct_niss_auto_load.sql
+++ F92157/models/marts/fct_niss_auto_load.sql
@@ -12,15 +12,15 @@ farmers_state_mapping AS (
         STATE_CODE
     FROM {{ ref('int_ref_tfarmers_state') }}
 )
+
 SELECT 
-    {{ dbt_utils.surrogate_key(['FARMERS_STATE_NAME', 'FARMERS_STATE_CD']) }} AS unique_id,
     niss_state_mapping.NISS_STATE_CODE,
     farmers_state_mapping.STATE_CODE,
-    DECODE(1, 
-        ISNULL(niss_state_mapping.NISS_STATE_CODE), '?', 
-        IS_SPACES(niss_state_mapping.NISS_STATE_CODE), '?', 
-        LTRIM(RTRIM(niss_state_mapping.NISS_STATE_CODE))
-    ) AS TERR_CD
+    CASE 
+        WHEN niss_state_mapping.NISS_STATE_CODE IS NULL THEN '?'
+        WHEN TRIM(niss_state_mapping.NISS_STATE_CODE) = '' THEN '?'
+        ELSE TRIM(niss_state_mapping.NISS_STATE_CODE)
+    END AS TERR_CD
 FROM niss_state_mapping
 LEFT JOIN farmers_state_mapping
-ON niss_state_mapping.FARMERS_STATE_NAME = farmers_state_mapping.STATE_CODE
+    ON niss_state_mapping.FARMERS_STATE_NAME = farmers_state_mapping.STATE_CODE
